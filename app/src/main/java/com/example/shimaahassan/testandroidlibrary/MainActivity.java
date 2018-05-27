a170df2d1bac73d909780fce213ae4aee2e9ae8e
Amira Mostafa <amira.mostafa@tagipedia.com>
2018-05-27 11:08:52 +0200

    - location permission dialog

--- app/src/main/java/com/example/shimaahassan/testandroidlibrary/MainActivity.java
+++ app/src/main/java/com/example/shimaahassan/testandroidlibrary/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.shimaahassan.testandroidlibrary;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.tagipedia.TUtils;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
             System.out.println("Topic " + bundle.getSerializable("topic"));
         }
         TUtils.showBluetoothDialog(this, "HELLLO" , "WORLD");
+        TUtils.showLocationDialog(this, "Location Permission", "get location permission");
     }
 
 }
