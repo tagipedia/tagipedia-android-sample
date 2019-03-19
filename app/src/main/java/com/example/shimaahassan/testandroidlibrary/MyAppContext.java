dcdef1156084fdc09693e4399075557a461fe22a
Khaled Ahmed <khaled.ahmed@tagipedia.com>
2019-03-19 11:38:29 +0200

    add features 	- navigation to map location from map 	- show ads based on user interests

--- app/src/main/java/com/example/shimaahassan/testandroidlibrary/MyAppContext.java
+++ app/src/main/java/com/example/shimaahassan/testandroidlibrary/MyAppContext.java
@@ -27,6 +27,24 @@ public class MyAppContext extends MultiDexApplication {
             }
         };
         tBuilder.setEventLoggerListener(loggedEventRecordListener);
+        Callback.OnMapButtonPressedListener onMapButtonPressedListener = new Callback.OnMapButtonPressedListener() {
+            @Override
+            public void onMapButtonPressed(HashMap hashMap) {
+                System.out.print(hashMap);
+                // dispatch to tagipedia maps to navigate to location should be like this
+                // LinkedHashMap<String, Object> navigationParams = new LinkedHashMap<String, Object>();
+                // navigationParams.put("route_to", (String)hashMap("booth_id"));
+                // new HashMap<String, Object>(){{
+                // put("type", "SHOW_NAVIGATION_DIALOG");
+                // put("navigation_params", navigationParams);
+                // }}
+            }
+        };
+        tBuilder.setMapButtonPressedListener(onMapButtonPressedListener);
         tBuilder.build();
+        //to register user with interests
+        //this will show ads based on matching between ad interests and user interests otherwise it will show ads that was created without interests
+        //String[] interests;
+        tBuilder.identifyUser("USER_NAME","UUID", interests);
     }
 }
